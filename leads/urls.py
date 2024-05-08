08bd3f5a3917025b7b6a843bfac26acc176aa14f
Akshat Nigam <akshatnigamcfl@gmail.com>
2024-05-08 17:12:35 +0530

    5/7/2024

--- leads/urls.py
+++ leads/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
             path('view_lead_search/approval/<str:approval_type>/<str:lead_id>', viewAllLeadsApprovalSearch.as_view()), #view_all_leads
             path('view_all_leads/approval/<str:approval_type>/<int:page>', viewAllLeadsApproval.as_view()), #view_all_leads
             path('approve_commercial/<str:approval_type>/<str:lead_id>', approveCommercial.as_view()), #view_all_leads
-            path('reject_commercial/<str:approval_type>', rejectCommercial.as_view()), #view_all_leads
+            path('reject_commercial/<str:approval_type>/<str:lead_id>', rejectCommercial.as_view()), #view_all_leads
 
             path('view_all_services/<str:client_id>', viewAllservices.as_view()), #view_all_leads
 
